406b4599f63fbdf934ff800e734a0d6bc1992954
prakash <prakashsundaram83@gmail.com>
2022-09-30 13:19:25 +0530

    procfile added

--- app/main.py
+++ app/main.py
@@ -5,6 +5,10 @@ from .booking.routers import user
 
 app = FastAPI()
 
+@app.get('/')
+def index():
+    return "Hello Prakash!"
+
 app.include_router(user.router)
 
 models.Base.metadata.create_all(engine)
